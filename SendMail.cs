3ae8ab0e8792c9081ca3dae89bff6b5045a4b21a
Serkan Kumru <serkankumru1997@gmail.com>
2023-09-16 11:58:17 +0300

    Add files via upload

---
+++ SendMail.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+
+namespace Haberler.Mail
+{
+    public class SendMail
+    {
+        public static void Send(string to, string subject, string body)
+        {
+            SmtpClient sc = new SmtpClient();
+            sc.Port = 587;
+            sc.Host = "smtp.gmail.com";
+            sc.EnableSsl = true;
+
+            sc.Credentials = new NetworkCredential("newsayvansaray@gmail.com", "habersitesi");
+
+            MailMessage mail = new MailMessage();
+
+            mail.From = new MailAddress("newsayvansaray@gmail.com", "Ayvansaray Haber");
+
+            mail.To.Add(to);
+            
+
+            mail.Subject = subject;
+            mail.IsBodyHtml = true;
+            mail.Body = body;
+
+
+            sc.Send(mail);
+        }
+
+        public static void Send(List<string> to, string subject, string body)
+        {
+            SmtpClient sc = new SmtpClient();
+            sc.Port = 587;
+            sc.Host = "smtp.gmail.com";
+            sc.EnableSsl = true;
+
+            sc.Credentials = new NetworkCredential("newsayvansaray@gmail.com", "habersitesi");
+
+            MailMessage mail = new MailMessage();
+
+            mail.From = new MailAddress("newsayvansaray@gmail.com", "Ayvansaray Haber");
+
+            foreach (var item in to)
+            {
+                mail.To.Add(item);
+            }
+
+            mail.Subject = subject;
+            mail.IsBodyHtml = true;
+            mail.Body = body;
+
+
+            sc.Send(mail);
+        }
+    }
+}
